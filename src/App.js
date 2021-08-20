a748ab95fb53f36b86db5d75b124c30cea3bd61e
Oshae Alexis <terongp3@gmail.com>
2021-08-20 02:29:26 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-
+const api = {
+  key: "f67490d0dd2daf9963641a5f4fda7e7b",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 function App() {
+   const [query, setQuery] = useState('');
+   const [weather, setWeather] = useState({});
+
+   const search = evt => {
+     if (evt.key === "Enter") {
+       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+         .then(res => res.json())
+       
+     }
+   }
+  
+   const dateBuilder = (d) =>{
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${month}  ${date} ${year}` 
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..."/>
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            70°F
+          </div>
+          <div className="weather">Sunny</div>
+        </div>
+      </main>
+ 
     </div>
   );
 }
